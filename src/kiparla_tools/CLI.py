19e3571039bc376d6dfb2fcdbb1879e97b00a40b
Ludovica Pannitto <ellepannitto@gmail.com>
2025-02-13 11:33:04 +0100

    old CLI

--- src/kiparla_tools/CLI.py
+++ src/kiparla_tools/CLI.py
@@ -73,98 +73,98 @@ def _align(args):
 
 
 
-if __name__ == "__main__":
-	### MAIN ###
 
-	parent_parser = argparse.ArgumentParser(add_help=False)
-	root_parser = argparse.ArgumentParser(prog='kiparla-tools', add_help=True)
-	subparsers = root_parser.add_subparsers(title="actions", dest="actions")
+### MAIN ###
 
-	# EAF2CSV
-	parser_eaf2csv = subparsers.add_parser("eaf2csv", parents=[parent_parser],
-											description='transform eaf file into csv',
-											help='transform eaf file into csv')
-	parser_eaf2csv.add_argument("-o", "--output-dir", default="output/",
-								type=ac.valid_dirpath,
-								help="path to output directory")
-	group = parser_eaf2csv.add_argument_group('Input files')
-	command_group = group.add_mutually_exclusive_group(required=True)
-	command_group.add_argument("--input-files", nargs="+",
-								type=ac.valid_filepath,
-								help="path(s) to eaf file(s)")
-	command_group.add_argument("--input-dir", default="input_eaf/",
-								type=ac.valid_dirpath,
-								help="path to input directory. All .eaf files will be transformed")
-	parser_eaf2csv.set_defaults(func=_eaf2csv)
+parent_parser = argparse.ArgumentParser(add_help=False)
+root_parser = argparse.ArgumentParser(prog='kiparla-tools', add_help=True)
+subparsers = root_parser.add_subparsers(title="actions", dest="actions")
 
-	# CSV2EAF
-	parser_csv2eaf = subparsers.add_parser("csv2eaf", parents=[parent_parser],
-											description='transform csv file into eaf',
-											help='transform csv file into eaf')
-	parser_csv2eaf.add_argument("-o", "--output-dir", default="output_eaf/",
-								type=ac.valid_dirpath,
-								help="path to output directory")
-	parser_csv2eaf.add_argument("-a", "--audio-dir", default="audio/",
-								type=ac.valid_dirpath,
-								help="path to directory containing audio files")
-	group = parser_csv2eaf.add_argument_group('Input files')
-	command_group = group.add_mutually_exclusive_group(required=True)
-	command_group.add_argument("--input-files", nargs="+",
-								type=ac.valid_filepath,
-								help="path(s) to eaf file(s)")
-	command_group.add_argument("--input-dir",
-								type=ac.valid_dirpath,
-								help="path to input directory. All .eaf files will be transformed")
-	parser_csv2eaf.add_argument("-d", "--delimiter", type=str, default="\t",
-								help="") #TODO: ADD HELP
-	parser_csv2eaf.add_argument("-m", "--multiplier-factor", type=int, default=1000,
-								help="") #TODO: ADD HELP
-	parser_csv2eaf.add_argument("-i", "--include-ids", action="store_true",
-								help="") #TODO write help
-	parser_csv2eaf.set_defaults(func=_csv2eaf)
+# EAF2CSV
+parser_eaf2csv = subparsers.add_parser("eaf2csv", parents=[parent_parser],
+										description='transform eaf file into csv',
+										help='transform eaf file into csv')
+parser_eaf2csv.add_argument("-o", "--output-dir", default="output/",
+							type=ac.valid_dirpath,
+							help="path to output directory")
+group = parser_eaf2csv.add_argument_group('Input files')
+command_group = group.add_mutually_exclusive_group(required=True)
+command_group.add_argument("--input-files", nargs="+",
+							type=ac.valid_filepath,
+							help="path(s) to eaf file(s)")
+command_group.add_argument("--input-dir", default="input_eaf/",
+							type=ac.valid_dirpath,
+							help="path to input directory. All .eaf files will be transformed")
+parser_eaf2csv.set_defaults(func=_eaf2csv)
 
-	# PROCESS
-	parser_process = subparsers.add_parser("process", parents=[parent_parser],
-											description='process transcripts',
-											help='process transcripts')
-	parser_process.add_argument("-o", "--output-dir", default="output_eaf/",
-								type=ac.valid_dirpath,
-								help="path to output directory")
-	group = parser_process.add_argument_group('Input files')
-	command_group = group.add_mutually_exclusive_group(required=True)
-	command_group.add_argument("--input-files", nargs="+",
-								type=ac.valid_filepath,
-								help="path(s) to eaf file(s)")
-	command_group.add_argument("--input-dir", default="input_csv/",
-								type=ac.valid_dirpath,
-								help="path to input directory. All .csv files will be transformed")
-	parser_process.add_argument("-t", "--duration-threshold", type=float, default=0.1,
-								help="") # TODO: write help
-	parser_process.add_argument("-s", "--produce-stats", action="store_true",
-								help="") # TODO: write help
-	parser_process.set_defaults(func=_process)
+# CSV2EAF
+parser_csv2eaf = subparsers.add_parser("csv2eaf", parents=[parent_parser],
+										description='transform csv file into eaf',
+										help='transform csv file into eaf')
+parser_csv2eaf.add_argument("-o", "--output-dir", default="output_eaf/",
+							type=ac.valid_dirpath,
+							help="path to output directory")
+parser_csv2eaf.add_argument("-a", "--audio-dir", default="audio/",
+							type=ac.valid_dirpath,
+							help="path to directory containing audio files")
+group = parser_csv2eaf.add_argument_group('Input files')
+command_group = group.add_mutually_exclusive_group(required=True)
+command_group.add_argument("--input-files", nargs="+",
+							type=ac.valid_filepath,
+							help="path(s) to eaf file(s)")
+command_group.add_argument("--input-dir",
+							type=ac.valid_dirpath,
+							help="path to input directory. All .eaf files will be transformed")
+parser_csv2eaf.add_argument("-d", "--delimiter", type=str, default="\t",
+							help="") #TODO: ADD HELP
+parser_csv2eaf.add_argument("-m", "--multiplier-factor", type=int, default=1000,
+							help="") #TODO: ADD HELP
+parser_csv2eaf.add_argument("-i", "--include-ids", action="store_true",
+							help="") #TODO write help
+parser_csv2eaf.set_defaults(func=_csv2eaf)
 
-	# ALIGN
-	parser_align = subparsers.add_parser("align", parents=[parent_parser],
-											description='align transcripts',
-											help='align transcripts')
-	parser_align.add_argument("-o", "--output-dir", default="output_aligned/",
-								type=ac.valid_dirpath,
-								help="path to output directory")
-	group = parser_align.add_argument_group('Input files')
-	command_group = group.add_mutually_exclusive_group(required=True)
-	command_group.add_argument("--input-files", nargs="+",
-								type=ac.valid_filepath,
-								help="path(s) to conllu file(s)")
-	command_group.add_argument("--input-dir", default="input_conllu/",
-								type=ac.valid_dirpath,
-								help="path to input directory. All .conllu files will be transformed")
-	parser_align.set_defaults(func=_align)
+# PROCESS
+parser_process = subparsers.add_parser("process", parents=[parent_parser],
+										description='process transcripts',
+										help='process transcripts')
+parser_process.add_argument("-o", "--output-dir", default="output_eaf/",
+							type=ac.valid_dirpath,
+							help="path to output directory")
+group = parser_process.add_argument_group('Input files')
+command_group = group.add_mutually_exclusive_group(required=True)
+command_group.add_argument("--input-files", nargs="+",
+							type=ac.valid_filepath,
+							help="path(s) to eaf file(s)")
+command_group.add_argument("--input-dir", default="input_csv/",
+							type=ac.valid_dirpath,
+							help="path to input directory. All .csv files will be transformed")
+parser_process.add_argument("-t", "--duration-threshold", type=float, default=0.1,
+							help="") # TODO: write help
+parser_process.add_argument("-s", "--produce-stats", action="store_true",
+							help="") # TODO: write help
+parser_process.set_defaults(func=_process)
 
-	args = root_parser.parse_args()
+# ALIGN
+parser_align = subparsers.add_parser("align", parents=[parent_parser],
+										description='align transcripts',
+										help='align transcripts')
+parser_align.add_argument("-o", "--output-dir", default="output_aligned/",
+							type=ac.valid_dirpath,
+							help="path to output directory")
+group = parser_align.add_argument_group('Input files')
+command_group = group.add_mutually_exclusive_group(required=True)
+command_group.add_argument("--input-files", nargs="+",
+							type=ac.valid_filepath,
+							help="path(s) to conllu file(s)")
+command_group.add_argument("--input-dir", default="input_conllu/",
+							type=ac.valid_dirpath,
+							help="path to input directory. All .conllu files will be transformed")
+parser_align.set_defaults(func=_align)
 
-	if "func" not in args:
-		root_parser.print_usage()
-		exit()
+args = root_parser.parse_args()
 
-	args.func(args)
+if "func" not in args:
+	root_parser.print_usage()
+	exit()
+
+args.func(args)
