64166d6da1374ae17de9d226be470f71cde5fa72
Ludovica Pannitto <ellepannitto@gmail.com>
2025-06-23 11:16:27 +0200

    very patchy version for parsing

--- src/kiparla_tools/linguistic_pipeline.py
+++ src/kiparla_tools/linguistic_pipeline.py
@@ -4,7 +4,8 @@ import kiparla_tools.serialize as serialize
 import kiparla_tools.alignment as alignment
 
 from kiparla_tools.config_parameters import (
-	CONLL_FIELDNAMES
+	CONLL_FIELDNAMES,
+	MULTIWORDS
 )
 
 #TODO: move into data
@@ -26,7 +27,7 @@ def parse(model, filename, output_filename, ignore_meta):
 
 		writer = csv.DictWriter(fout, delimiter="\t", fieldnames=fieldnames, restval="_")
 		writer.writeheader()
-
+		print("FILENAME", filename)
 		for unit_id, unit in serialize.units_from_conll(fin):
 			unit_processed = []
 			unit_text = []
@@ -35,16 +36,18 @@ def parse(model, filename, output_filename, ignore_meta):
 				tok_type = token["type"]
 				text = token["form"]
 
-				if ignore_meta and not tok_type in ["shortpause", "metalinguistic"]:
+				if ignore_meta and not tok_type in ["shortpause", "nonverbalbehavior"]:
 					unit_text.append(text) #TODO: handle spaceafter?
 				unit_full.append(text)
 
 			if len(unit_text):
 				doc = model(" ".join(unit_text))
+				print()
+				print()
+				print(doc)
 
 				doc = [token for token in doc]
 				doc_text = [token.text for token in doc]
-
 
 				# unit_full = il gatto ((sbadiglio)) dorme sul divano
 				# unit_text = il gatto dorme sul divano
@@ -54,9 +57,6 @@ def parse(model, filename, output_filename, ignore_meta):
 
 				aligned_orig, aligned_proces, _, _ = alignment.align(unit_full, doc_text)
 
-				for a, b in zip(aligned_orig, aligned_proces):
-					print(a, b)
-
 				subparts = []
 				i=0
 				part = []
@@ -64,6 +64,7 @@ def parse(model, filename, output_filename, ignore_meta):
 					while i<len(aligned_orig) and aligned_orig[i] == aligned_proces[i]:
 						part.append(i)
 						i+=1
+
 					if len(part)>0:
 						subparts.append(["ALIGNED", part])
 						part = []
@@ -71,20 +72,77 @@ def parse(model, filename, output_filename, ignore_meta):
 					while i<len(aligned_orig) and aligned_orig[i] != aligned_proces[i]:
 						part.append(i)
 						i+=1
+
 					if len(part)>0:
 						subparts.append(["MISMATCH", part])
 						part = []
-					i+=1
-					# print("PARTIAL", subparts)
-					# input()
+					# i+=1
+
+				# print(subparts)
+				aligned_sequence = []
 
 				for element in subparts:
 					label, element_ids = element
-					if label == "MISMATCH":
+					print(label, [aligned_orig[i] for i in element_ids])
+					print(label, [aligned_proces[i] for i in element_ids])
+
+					if label == "ALIGNED":
+						for i in element_ids:
+							aligned_sequence.append(("original", aligned_orig[i]))
+
+					elif label == "MISMATCH":
+						# print("HERE")
+
 						orig_elements = [aligned_orig[i] for i in element_ids if not aligned_orig[i] == "_"]
 						proces_elements = [aligned_proces[i] for i in element_ids if not aligned_proces[i] == "_"]
-						assert(len(orig_elements)>0)
 
+						# print(orig_elements)
+						# print(proces_elements)
+
+						if len(orig_elements) == 0:
+							aligned_sequence.append(("error", " ".join(proces_elements)))
+
+						else:
+							# print(orig_elements, proces_elements)
+							# print("HERE")
+							i=0
+							j=0
+
+							while i<len(orig_elements):
+								tok = orig_elements[i]
+								# print(tok)
+								if tok.startswith("{"):
+									aligned_sequence.append(("nvb", tok))
+								else:
+									print(tok in MULTIWORDS, len(proces_elements), j)
+									if tok in MULTIWORDS:
+										if j+MULTIWORDS[tok]<len(proces_elements)+1:
+											aligned_sequence.append(("multiword-B", tok))
+											aligned_sequence.append(("multiword-I", proces_elements[j]))
+											# print("multi", proces_elements[j])
+											# print(tok, proces_elements[j])
+											for _ in range(MULTIWORDS[tok]-1):
+												j+=1
+												aligned_sequence.append(("multiword-I", proces_elements[j]))
+												# print("_", proces_elements[j])
+											j+=1
+										else:
+											aligned_sequence.append(("original", proces_elements[j]))
+											j+=1
+									else:
+										print("############# TOK ##############")
+										print(tok)
+										#TODO
+										aligned_sequence.append(("original", proces_elements[j]))
+										aligned_sequence.append(("ignore", proces_elements[j+1]))
+										j+=2
+								i+=1
+
+				print("ALIGNED SEQUENCE:")
+				print(aligned_sequence)
+				# input()
+
+				# OLD VERSION, REMOVE
 						# orig_elements_len = []
 						# for el in orig_elements:
 						# 	if el.startswith("{"):
@@ -99,62 +157,138 @@ def parse(model, filename, output_filename, ignore_meta):
 				# 		aligned_orig[i], aligned_orig[j] = aligned_orig[j], aligned_orig[i]
 				# 	i-=1
 
+				# OLD VERSION, REMOVE
 				idx_unit, idx_doc = 0, 0
-				ids = []
+				last_mw_id = ""
+				last_mv_idx = 0
+				mw_suffix = ["a", "b", "c", "d"]
 				ptr = None
-				for _, (orig_token, proces_token) in enumerate(zip(aligned_orig, aligned_proces)):
-					updatable = True
-					if orig_token == proces_token:
-						add_info(unit[idx_unit], doc[idx_doc])
-						unit_processed.append(unit[idx_unit])
 
+				for alignment_type, token in aligned_sequence:
+					print(alignment_type, token)
+
+					if alignment_type == "nvb":
+						print("adding", unit[idx_unit])
+						unit_processed.append(unit[idx_unit])
+						idx_unit += 1
+
+					if alignment_type == "ignore":
+						print("adding nothing")
+						new_token = {"token_id": f"{unit_id}-X",
+									"unit": unit_id,
+									"form": "_"}
+						add_info(new_token, doc[idx_doc])
+						unit_processed.append(new_token)
+						# idx_unit += 1
+						idx_doc += 1
+
+					if alignment_type == "original":
+						add_info(unit[idx_unit], doc[idx_doc])
+						print("adding", unit[idx_unit])
+						unit_processed.append(unit[idx_unit])
 						idx_unit += 1
 						idx_doc += 1
 
-					elif proces_token == "_":
+					if alignment_type == "error":
+						new_token = {"token_id": unit[idx_unit]["token_id"],
+									"unit": unit_id,
+									"form": doc[idx_doc].text}
+						add_info(new_token, doc[idx_doc])
+						unit_processed.append(new_token)
+						# print(token)
+						idx_doc += 1
+
+					if alignment_type == "multiword-B":
+						last_mw_id = unit[idx_unit]["token_id"]
+						last_mv_idx = 0
+						unit[idx_unit]["id"] = "100-0"
+						ptr = unit[idx_unit]
+						print("adding", unit[idx_unit])
 						unit_processed.append(unit[idx_unit])
 						idx_unit += 1
 
-					elif orig_token == "_":
-						updatable = False
-						new_token = {"token_id":"_",
+					if alignment_type == "multiword-I":
+						new_token = {"token_id": f"{last_mw_id}{mw_suffix[last_mv_idx]}",
 									"unit": unit_id,
 									"form": doc[idx_doc].text}
-						ids.append(doc[idx_doc].i+1)
+						last_mv_idx +=1
 						add_info(new_token, doc[idx_doc])
+						print("adding", new_token)
 						unit_processed.append(new_token)
 						idx_doc += 1
 
-					else:
-						updatable = False
-						#TODO: aiutooooooo
-						ids.append(doc[idx_doc].i+1)
-						new_token = {"token_id":"_",
-									"unit": unit_id,
-									"form": doc[idx_doc].text}
-						add_info(new_token, doc[idx_doc])
+						old_ids = ptr["id"].split("-")
+						id_min, id_max = int(old_ids[0]), int(old_ids[1])
+						curr_id = int(new_token["id"])
+						if curr_id < id_min:
+							id_min = curr_id
+						if curr_id > id_max:
+							id_max = curr_id
+						ptr["id"] = f"{id_min}-{id_max}"
 
-						if orig_token.startswith("{"):
-							unit_processed.append(new_token)
-							unit_processed.append(unit[idx_unit])
-						else:
-							# updatable = False
-							ptr = unit[idx_unit]
-							unit_processed.append(unit[idx_unit])
-							unit_processed.append(new_token)
+				for token in unit_processed:
+					print(token["token_id"], token["id"], token["form"], token["deprel"])
+					# input()
+				# print(unit_processed)
+				# input("END")
 
-						idx_unit += 1
-						idx_doc += 1
+				# OLD VERSION, REMOVE
+				# idx_unit, idx_doc = 0, 0
+				# ids = []
+				# ptr = None
+				# for _, (orig_token, proces_token) in enumerate(zip(aligned_orig, aligned_proces)):
+				# 	updatable = True
+				# 	if orig_token == proces_token:
+				# 		add_info(unit[idx_unit], doc[idx_doc])
+				# 		unit_processed.append(unit[idx_unit])
 
-					if updatable and ptr:
-						ptr["id"] = "-".join([str(x) for x in ids])
-						ids = []
-						ptr = None
+				# 		idx_unit += 1
+				# 		idx_doc += 1
 
-				if updatable and ptr:
-					ptr["id"] = "-".join([str(x) for x in ids])
-					ids = []
-					ptr = None
+				# 	elif proces_token == "_":
+				# 		unit_processed.append(unit[idx_unit])
+				# 		idx_unit += 1
+
+				# 	elif orig_token == "_":
+				# 		updatable = False
+				# 		new_token = {"token_id":"_",
+				# 					"unit": unit_id,
+				# 					"form": doc[idx_doc].text}
+				# 		ids.append(doc[idx_doc].i+1)
+				# 		add_info(new_token, doc[idx_doc])
+				# 		unit_processed.append(new_token)
+				# 		idx_doc += 1
+
+				# 	else:
+				# 		updatable = False
+				# 		#TODO: aiutooooooo
+				# 		ids.append(doc[idx_doc].i+1)
+				# 		new_token = {"token_id":"_",
+				# 					"unit": unit_id,
+				# 					"form": doc[idx_doc].text}
+				# 		add_info(new_token, doc[idx_doc])
+
+				# 		if orig_token.startswith("{"):
+				# 			unit_processed.append(new_token)
+				# 			unit_processed.append(unit[idx_unit])
+				# 		else:
+				# 			# updatable = False
+				# 			ptr = unit[idx_unit]
+				# 			unit_processed.append(unit[idx_unit])
+				# 			unit_processed.append(new_token)
+
+				# 		idx_unit += 1
+				# 		idx_doc += 1
+
+				# 	if updatable and ptr:
+				# 		ptr["id"] = "-".join([str(x) for x in ids])
+				# 		ids = []
+				# 		ptr = None
+
+				# if updatable and ptr:
+				# 	ptr["id"] = "-".join([str(x) for x in ids])
+				# 	ids = []
+				# 	ptr = None
 
 				root_id = -1
 				ids_map = {}
@@ -169,27 +303,40 @@ def parse(model, filename, output_filename, ignore_meta):
 							root_id = new_id
 						new_id += 1
 
-				# print(ids_map)
 				for token in unit_processed:
 					if not token["deprel"] == "root:0":
 						if token["deprel"] == "_":
 							token["deprel"] = "dep:" + str(root_id)
 						else:
-							# print(token)
+				# 			# print(token)
 							token["deprel"] = ":".join([token["deprel"].rsplit(":", 1)[0], str(ids_map[token["deprel"].rsplit(":", 1)[1]])])
 					if "-" in token["id"]:
 						token["id"] = "-".join([str(ids_map[x]) for x in token["id"].split("-")])
 						token["deprel"] = "_"
-						# token["deprel"] = token["deprel"].split(":")[0] + ":" + str(root_id)
-					# print(token["id"], token["form"], token["deprel"])
-				# input()
+
+				for token in unit_processed:
+					print(token["token_id"], token["id"], token["form"], token["deprel"])
+					# input()
+				# print(unit_processed)
+				# input("END")
+
 				final_list.extend(unit_processed)
 
 			else:
 				final_list.extend(unit)
 
 		for token in final_list:
+			# print(token)
+			# input()
+			if "type" in token and token["type"] == "nonverbalbehavior":
+				if token["id"] == "_":
+					token["id"] = token["token_id"].split("-")[1]
+					token["upos"] = "X"
+					token["deprel"] = "root:0"
+
+			# if not token["token_id"] == "_":
 			writer.writerow(token)
+
 
 def segment(model, filename, output_filename, ignore_meta):
 
@@ -204,7 +351,7 @@ def segment(model, filename, output_filename, ignore_meta):
 			type = row["type"]
 			text = row["form"]
 
-			if ignore_meta and not type in ["error", "shortpause", "metalinguistic"]:
+			if ignore_meta and not type in ["error", "shortpause", "nonverbalbehavior"]:
 				full_text.append(text)
 				full_ids.append(token_id)
 
